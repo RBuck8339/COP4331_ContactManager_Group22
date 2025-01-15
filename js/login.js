14001b816a1c11f30f872af602c52684268fa800
RBuck8339 <101837795+RBuck8339@users.noreply.github.com>
2025-01-15 12:18:11 -0500

    Potential formatting fixes. Need to check live server php code

--- js/login.js
+++ js/login.js
@@ -1,18 +1,18 @@
-const loginBox = document.getElementById('loginMenu');
-const registerBox = document.getElementById('registerMenu');
+const loginMenu = document.getElementById('loginMenu');
+const registerMenu = document.getElementById('registerMenu');
 
 // Function to toggle between login and register forms
 const toggleScreen = (showScreenLogin) => {
     // Could add some more reactivity when switching between screens (such as elements fading upwards)
     if(showScreenLogin) {
-        loginBox.classList.remove('hidden');
-        registerBox.classList.add('hidden');
+        loginMenu.classList.remove('hidden');
+        registerMenu.classList.add('hidden');
         document.getElementById('toLoginBtn').classList.add('activePage'); // Correct button ID
         document.getElementById('toRegisterBtn').classList.remove('activePage');
     }
     else {
-        loginBox.classList.add('hidden');
-        registerBox.classList.remove('hidden');
+        loginMenu.classList.add('hidden');
+        registerMenu.classList.remove('hidden');
         document.getElementById('toRegisterBtnReg').classList.add('activePage'); // Correct button ID
         document.getElementById('toLoginBtnReg').classList.remove('activePage');
     }
