a98874baedd0a073c12ccc93c79f211babcd7455
RedNixTV <ricky4912@gmail.com>
2025-01-29 19:10:00 -0500

    renamed addUser.php to reger.php

---
+++ LAMPAPI/Register.php
@@ -0,0 +1,55 @@
+<?php
+	$inData = getRequestInfo();
+
+ 	$firstName = $inData["firstName"];
+ 	$lastName = $inData["lastName"];
+ 	$login = $inData["login"];
+  	$password = $inData["password"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331","contact_manager");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("SELECT ID FROM Users WHERE Login=?");
+		$stmt->bind_param("s", $login);
+		$stmt->execute();
+		$stmt->store_result();
+		if($stmt->num_rows > 0)
+		{
+			$stmt->close();
+			$conn->close();
+			returnWithError("User already exists.");
+		}
+		else
+		{
+		$stmt->close();
+		$stmt = $conn->prepare("INSERT INTO Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
+		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+		}
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
